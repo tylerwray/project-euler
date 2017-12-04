edfc236e4744a56c8f19a2711c6bf5a994280420
Tyler Wray <wraytw@gmail.com>
2017-12-04 16:54:26 -0700

    First four projects

---
+++ 3_largest_prime_factor.py
@@ -0,0 +1,32 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+
+# What is the largest prime factor of the number 600851475143 ?
+
+def largestPrimeFactor(number):
+  largest = 0
+  i = 2
+
+  while number > 1:
+    if number % i == 0:
+      largest = i
+      number = number / i
+    i += 1
+  return largest
+
+
+print largestPrimeFactor(600851475143)
+
+"""
+### Tests ###
+
+if largestPrimeFactor(4) != 2:
+  raise AssertionError('Largest Prime Factor Failed')
+
+if largestPrimeFactor(6) != 3:
+  raise AssertionError('Largest Prime Factor Failed')
+
+if largestPrimeFactor(13195) != 29:
+  raise AssertionError('Largest Prime Factor Failed')
+
+print 'Success!'
+"""
---
+++ 4_largest_palindrome_product.py
@@ -0,0 +1,35 @@
+# A palindromic number reads the same both ways.
+# The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 * 99
+
+# Find the largest palindrome made from the product of two 3 digit numbers.
+
+def isPalindrome(string):
+    start = 0
+    end = len(string) - 1
+
+    while start < end:
+        if string[start] != string[end]:
+            return False
+        else:
+            end -= 1
+            start += 1
+
+    return True
+
+def largestThreeDigitPalindrome():
+    first = 999
+    second = 999
+
+    largest = 0
+
+    while first != 100:
+        while second != 100:
+            product = first * second
+            if isPalindrome(str(product)) and product > largest:
+                largest = product
+            second -= 1
+        second = 999
+        first -= 1
+    return largest
+
+print largestThreeDigitPalindrome()
